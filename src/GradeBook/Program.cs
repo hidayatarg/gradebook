5942eb1c6357a7052fb853a8d1db72fef616a04e
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-07-18 18:15:04 +0300

    Created a List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,11 +8,22 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var numbers = new []{12,15.6};
+            List<int> grades = new List<int>();
+            grades.Add(12);
+            grades.Add(11);
+            grades.Add(10);
             double result = 0;
             foreach (var item in numbers)
             {
                 result += item;
             }
+
+            // print list 
+            foreach (var item in grades.FindAll(x => x > 11))
+            {
+                Console.WriteLine(item);
+            }
+            System.Console.WriteLine("\n\n");
             Console.WriteLine(result);
         }
     }
