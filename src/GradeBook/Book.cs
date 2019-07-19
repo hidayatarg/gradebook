05260576a37f4864e855224d065e54ddbcbc1a73
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-07-19 11:09:42 +0300

    Abstruction - Refactor the Methods

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -14,11 +15,32 @@ namespace GradeBook
         {
             grades.Add(grade);
         }
-        private List<double> grades;
-        private string name;
-        public List<double> GetGrades()
+         public void ShowStatistics()
+        {
+            double result = 0;
+            double highGrade = double.MinValue;
+            double lowGrade = double.MinValue;
+            foreach (var item in grades)
+            {
+                lowGrade = Math.Min(item, highGrade);
+                highGrade = Math.Max(item, highGrade);
+                result += item;
+            }
+            double average = result/grades.Count;
+            // double highGrade = grades.Min();
+            // double highGrade = grades.Where(x => x <= 12);
+            // List<double> gradeTwelve = new List<double>();
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine($"Average: {average:N3}");
+            Console.WriteLine($"Max: {highGrade:N3}");
+            Console.WriteLine($"Min: {lowGrade:N3}");
+        }
+         public List<double> GetGrades()
         {
             return grades;
         }
+        private List<double> grades;
+        private string name;
+       
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -14,24 +14,9 @@ namespace GradeBook
             book.AddGrade(12);
             book.AddGrade(11);
             book.AddGrade(10);
+            book.ShowStatistics();
             var getGrades = book.GetGrades();
-            double result = 0;
-            double highGrade = double.MinValue;
-            double lowGrade = double.MinValue;
-            foreach (var item in getGrades)
-            {
-                lowGrade = Math.Min(item, highGrade);
-                highGrade = Math.Max(item, highGrade);
-                result += item;
-            }
-            double average = result/getGrades.Count;
-            // double highGrade = grades.Min();
-            // double highGrade = grades.Where(x => x <= 12);
-            // List<double> gradeTwelve = new List<double>();
-            Console.WriteLine($"Result: {result}");
-            Console.WriteLine($"Average: {average:N3}");
-            Console.WriteLine($"Max: {highGrade:N3}");
-            Console.WriteLine($"Min: {lowGrade:N3}");
+           
         }
     }
 }
