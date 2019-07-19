b3e73e29315e77b01146bbb78e1935fe7e2256b7
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-07-19 11:05:41 +0300

    Cleaned the Code

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -11,32 +11,23 @@ namespace GradeBook
 
             var book = new Book("Life Story");
             book.AddGrade(54);
+            book.AddGrade(12);
+            book.AddGrade(11);
+            book.AddGrade(10);
             var getGrades = book.GetGrades();
-            foreach (var item in getGrades)
-            {
-                System.Console.WriteLine($"Incoming Grade {item}");
-            }
-
-
-            var grades = new List<double>(){12,15.6 };
-            grades.Add(12);
-           
-           
-            // print list 
             double result = 0;
             double highGrade = double.MinValue;
             double lowGrade = double.MinValue;
-            foreach (var item in grades)
+            foreach (var item in getGrades)
             {
                 lowGrade = Math.Min(item, highGrade);
                 highGrade = Math.Max(item, highGrade);
                 result += item;
             }
-            double average = result/grades.Count;
+            double average = result/getGrades.Count;
             // double highGrade = grades.Min();
             // double highGrade = grades.Where(x => x <= 12);
             // List<double> gradeTwelve = new List<double>();
-            var gradeTwelve = grades.Where(x => x > 12);
             Console.WriteLine($"Result: {result}");
             Console.WriteLine($"Average: {average:N3}");
             Console.WriteLine($"Max: {highGrade:N3}");
